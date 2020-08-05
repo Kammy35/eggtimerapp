e4c6a9e3aec059a90c7f51b4954c3bb2a409b4d2
kammy35 <kammy007@hotmail.com>
2020-08-05 10:05:22 +0100

    Add project files.

---
+++ EggMobileApp/EggMobileApp/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using EggMobileApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EggMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public static TimeSpan EggTime;
+
+        private EggPageViewModel _viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            _viewModel = new EggPageViewModel();
+            BindingContext = _viewModel;
+
+            
+
+
+            EggSize.Items.Add("Small");
+            EggSize.Items.Add("Medium");
+            EggSize.Items.Add("Large");
+
+            EggTexture.Items.Add("Soft");
+            EggTexture.Items.Add("Medium");
+            EggTexture.Items.Add("Hard");
+
+            EggTexture.SelectedIndexChanged += (object sender, EventArgs e) =>
+            {
+                if (EggTexture.SelectedItem == "Soft")
+                {
+                    EggTime = CheckEggTexture("Soft");
+                    _viewModel.StartTime = EggTime;
+                    _viewModel.Duration = _viewModel.StartTime.ToString();
+
+                } else if (EggTexture.SelectedItem == "Medium")
+                {
+                    EggTime = CheckEggTexture("Medium");
+                    _viewModel.StartTime = EggTime;
+                    _viewModel.Duration = _viewModel.StartTime.ToString();
+                }
+                else
+                {
+                    EggTime = CheckEggTexture("Hard");
+                    _viewModel.StartTime = EggTime;
+                    _viewModel.Duration = _viewModel.StartTime.ToString();
+
+                }
+            };
+
+        }
+
+
+        public static TimeSpan CheckEggTexture(string EggTexture)
+        {
+            if (EggTexture == "Soft")
+            {
+                return  TimeSpan.FromSeconds(180);
+
+            }
+            else if (EggTexture == "Medium")
+            {
+                return TimeSpan.FromSeconds(360); ;
+            }
+            else  
+            {
+                return TimeSpan.FromSeconds(900);
+            }
+        }
+
+       
+
+
+        private void startButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetButton_Clicked(object sender, EventArgs e)
+        {
+            _viewModel.StartTime = TimeSpan.FromSeconds(0);
+            _viewModel.Duration = _viewModel.StartTime.ToString();
+        }
+
+       
+    }
+
+
+}
---
+++ EggMobileApp/EggMobileApp/ViewModel/EggPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EggMobileApp.ViewModel
+{
+    public class EggPageViewModel : INotifyPropertyChanged
+    {
+        private string _duration;
+
+        public EggPageViewModel()
+        {
+            StartTimerCommand = new Command(OnStartTimerExecute);
+            StartTime = TimeSpan.FromSeconds(0);
+            Duration = StartTime.ToString();
+        }
+
+        private void OnStartTimerExecute()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), (() =>
+            {
+                if (StartTime.TotalSeconds > 0)
+                {
+                    StartTime = StartTime - TimeSpan.FromSeconds(1);
+                    Duration = StartTime.ToString();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }));
+        }
+
+        
+        public ICommand StartTimerCommand { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public string Duration
+        {
+            get { return _duration; }
+            set 
+            {
+                _duration = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
